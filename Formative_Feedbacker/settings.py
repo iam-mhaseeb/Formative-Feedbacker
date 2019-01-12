ef2d8ea76f0bd6d1421109f63d50d320c1cb6f35
iam-mhaseeb <haseeb.emailbox@gmail.com>
2019-01-12 11:55:24 +0500

    Url Updated for Statics

--- Formative_Feedbacker/settings.py
+++ Formative_Feedbacker/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
